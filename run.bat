75ddc37f1ebe61c9f5db0c107e311ab36db97ffb
Nick Bartlett <nbartlett@shracing.com>
2023-06-06 13:37:34 -0400

    Generalize run folder location

--- run.bat
+++ run.bat
@@ -1,5 +1,7 @@
 :: @echo off
+set "run_directory=%userprofile%\FordWindTunnelPlotter"
+cd /d %run_directory%
 
-.\dist\env\Scripts\python.exe .\install_check.py
-.\dist\env\Scripts\python.exe .\dist\env\Scripts\voila.exe .\src\app.ipynb
+%run_directory%\dist\env\Scripts\python.exe %run_directory%\install_check.py
+%run_directory%\dist\env\Scripts\python.exe %run_directory%\dist\env\Scripts\voila.exe %run_directory%\src\app.ipynb
 pause
